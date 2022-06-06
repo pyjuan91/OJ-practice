0a8a59fda5aa5f83d09bdb174362b34f7cc04718
Poyu Juan <85391113+Poyuiu@users.noreply.github.com>
2022-06-07 01:12:27 +0800

    leetcode 160 accept

--- leetcode160.cpp
+++ leetcode160.cpp
@@ -1,36 +1,68 @@
 #include <iostream>
-#include <set>
 using namespace std;
-
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
 };
-
-class Solution
-{
+class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
-    {
-        set<ListNode *> s;
-        while(headA)
-        {
-            s.insert(headA);
-            headA = headA->next;
-        }
-        if(s.empty()) return nullptr;
-        while(headB)
-        {
-            if(s.find(headB) != s.end())
-            return headB;
-            headB = headB->next;
-        }
-        return nullptr;
+  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    while (headA != nullptr) {
+      ListNode *flag = headB;
+      while (flag != nullptr) {
+        if (flag == headA)
+          return headA;
+        else
+          flag = flag->next;
+      }
+      headA = headA->next;
     }
+    return headA;
+  }
 };
-int main(void)
-{
-    return 0;
-}
+
+class Solution {
+public:
+  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode *t1 = headA;
+    ListNode *t2 = headB;
+    int l1 = 1;
+    int l2 = 1;
+    while (t1->next) {
+      l1 += 1;
+      t1 = t1->next;
+    }
+    while (t2->next) {
+      l2 += 1;
+      t2 = t2->next;
+    }
+    if (t1 != t2) {
+      return NULL;
+    }
+    int move1 = max(0, l1 - l2);
+    int move2 = max(0, l2 - l1);
+    while (move1 > 0) {
+      headA = headA->next;
+      move1--;
+    }
+    while (move2 > 0) {
+      headB = headB->next;
+      move2--;
+    }
+    while (headA != headB) {
+      headB = headB->next;
+      headA = headA->next;
+    }
+    return headA;
+  }
+};
+int main(void) {}
