47013e20e670e19740cfcdb6f11c4196a92930c7
poyu <yoyohihirobert@gmail.com>
2022-07-14 19:47:41 +0800

    leetcode 509 accept

---
+++ leetcode/leetcode509.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int fib(int n)
+    {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+};
+int main(void)
+{
+    return 0;
+}
