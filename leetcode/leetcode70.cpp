a4f421bc98ed0695ce88e498aa4b7d891f80b902
poyu <yoyohihirobert@gmail.com>
2022-07-14 21:16:37 +0800

    leetcode 70 accept

---
+++ leetcode/leetcode70.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int climbStairs(int n)
+    {
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        int a = 1, b = 2, c;
+        for (int i = 3; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+};
+int main(void)
+{
+    Solution s;
+    cout << s.climbStairs(3) << endl;
+    return 0;
+}
