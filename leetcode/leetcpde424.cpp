5667b94bf6655e473238bdfb372d7333483e2aeb
poyu <yoyohihirobert@gmail.com>
2022-07-25 16:59:15 +0800

    leetcode 424 not yet

---
+++ leetcode/leetcpde424.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int characterReplacement(string s, int k)
+    {
+        char last = s[0];
+        int window[26], checker[26], remain = k, len_s = s.length(), maxLen = 0, len = 0;
+        for (int i = 0; i < len_s; i++) {
+            
+        }
+    }
+};
+int main(void)
+{
+    return 0;
+}
