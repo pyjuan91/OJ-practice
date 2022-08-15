5844d5f0df710e046bbc2382fd423e4826880395
poyu <yoyohihirobert@gmail.com>
2022-08-16 02:35:45 +0800

    leetcode 263 accept

---
+++ leetcode/leetcode263.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    bool isUgly(int n)
+    {
+        if (n < 1)
+            return false;
+        if (n == 1)
+            return true;
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        while (n % 5 == 0) {
+            n /= 5;
+        }
+        if (n == 1)
+            return true;
+        return false;
+    }
+};
+int main(void)
+{
+    return 0;
+}
