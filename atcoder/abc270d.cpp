4eee47fa93b9a0e9f73ece0a57bb285f86b75023
poyu <yoyohihirobert@gmail.com>
2022-09-25 03:26:39 +0800

    abc 270 d accept

--- atcoder/abc270d.cpp
+++ atcoder/abc270d.cpp
@@ -1,30 +1,21 @@
-#include <algorithm>
-#include <cstdio>
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-int a[105];
+int a[105], dp[10005];
 int main(void)
 {
-    int n, k, cur, turn = 1;
-    long long res = 0;
+    int n, k;
     scanf("%d%d", &n, &k);
-    for (int i = 0; i < k; i++) {
-        scanf("%d", &a[i]);
-    }
-    cur = k - 1;
-    while (n > 0) {
-        while (n < a[cur]) {
-            cur--;
+    for (int i = 0; i < k; i++)
+        scanf("%d", a + i);
+    dp[1] = 1;
+    dp[2] = a[1] == 2 ? 2 : 1;
+    for (int i = 3; i <= n; i++) {
+        int ma = INT_MIN;
+        for (int j = 0; j < k && a[j] <= i; j++) {
+            ma = max(ma, i - dp[i - a[j]]);
         }
-
-        if (turn == 1) {
-            res += (long long)a[cur];
-        }
-
-        turn = 1 - turn;
-        n -= a[cur];
+        dp[i] = ma;
     }
-    printf("%lld\n", res);
+    printf("%d\n", dp[n]);
     return 0;
 }
