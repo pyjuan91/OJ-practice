658c745918166b7af910fe20dd105eeb0d8c0f63
poyu <yoyohihirobert@gmail.com>
2022-10-24 19:45:22 +0800

    sm

---
+++ leetcode/leetcode33.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target)
+    {
+        
+    }
+};
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
