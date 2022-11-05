3f0c0951e73ffff05cbbacf7a26f19a3c0fe611e
poyu <yoyohihirobert@gmail.com>
2022-11-05 23:30:02 +0800

    meow w6-3 accept

--- meow/w6-3.cpp
+++ meow/w6-3.cpp
@@ -1,17 +1,16 @@
 #include <algorithm>
 #include <cstring>
+#include <deque>
 #include <iostream>
-#include <map>
 #include <queue>
 #include <string>
-#include <unordered_set>
 #include <vector>
 using namespace std;
 const int dx[4] = { 0, 0, 1, -1 };
 const int dy[4] = { 1, -1, 0, 0 };
-const string dir[4] = { "R", "L", "D", "U" };
-const string dirB[4] = { "L", "R", "U", "D" };
+const char dir[4] = { 'R', 'L', 'D', 'U' };
 char grid[1005][1005];
+int direction[1005][1005];
 int main(void)
 {
     ios_base::sync_with_stdio(false);
@@ -35,72 +34,57 @@ int main(void)
     for (int j = 0; j <= m + 1; j++)
         grid[0][j] = grid[n + 1][j] = '#';
 
+    grid[start.first][start.second] = '#';
     queue<pair<int, int>> point;
     queue<int> step;
-    queue<string> route;
-    map<pair<int, int>, int> A, B;
-    map<pair<int, int>, string> AA, BB;
+    // queue<string> route;
 
     point.push(start);
     step.push(0);
-    route.push("");
-    point.push(end);
-    step.push(0);
-    route.push("");
-    A[start] = 0;
-    B[end] = 0;
-    AA[start] = "";
-    BB[end] = "";
+    // route.push("");
 
     while (!point.empty()) {
         pair<int, int> cur_point = point.front();
-        bool isA
-            = grid[cur_point.first][cur_point.second] == 'A';
-        
-        int cur_step = isA?A[cur_point]:B[cur_point];
-        string cur_route = isA?AA[cur_point]:BB[cur_point];
+        int cur_step = step.front();
+        // string cur_route = route.front();
 
         point.pop();
+        step.pop();
+        // route.pop();
+
+        if (cur_point == end) {
+            cout << "YES\n"
+                 << cur_step << "\n";
+            //  << cur_route << "\n";
+            vector<char> res;
+            while (cur_point != start) {
+                res.push_back(dir[direction[cur_point.first][cur_point.second]]);
+                cur_point = { cur_point.first - dx[direction[cur_point.first][cur_point.second]],
+                    cur_point.second - dy[direction[cur_point.first][cur_point.second]] };
+            }
+            for (auto i = res.rbegin(); i != res.rend(); i++) {
+                cout << *i;
+            }
+            cout << "\n";
+            return 0;
+        }
+
+        // if (grid[cur_point.first][cur_point.second] == '#')
+        //     continue;
 
         for (int i = 0; i < 4; i++) {
-
             pair<int, int> new_point
                 = make_pair(cur_point.first + dx[i], cur_point.second + dy[i]);
-
-            if (isA && grid[new_point.first][new_point.second] == 'B') {
-                std::cout << "YES\n"
-                          << cur_step + B[new_point] + 1 << "\n"
-                          << cur_route + dir[i] + BB[new_point] << "\n";
-                return 0;
-            }
-
-            if (!isA && grid[new_point.first][new_point.second] == 'A') {
-                std::cout << "YES\n"
-                          << A[new_point] + cur_step + 1 << "\n"
-                          << AA[new_point] + dirB[i] + cur_route << "\n";
-                return 0;
-            }
-
-            if (grid[new_point.first][new_point.second] != '.')
+            if (grid[new_point.first][new_point.second] == '#')
                 continue;
+            grid[new_point.first][new_point.second] = '#';
             point.push(new_point);
             step.push(cur_step + 1);
-            if (isA) {
-                route.push(cur_route + dir[i]);
-                A[new_point] = cur_step + 1;
-                AA[new_point] = cur_route + dir[i];
-
-                grid[new_point.first][new_point.second] = 'A';
-            } else {
-                route.push(dirB[i] + cur_route);
-                B[new_point] = cur_step + 1;
-                BB[new_point] = dirB[i] + cur_route;
-
-                grid[new_point.first][new_point.second] = 'B';
-            }
+            direction[new_point.first][new_point.second] = i;
+            // route.push(cur_route + dir[i]);
         }
     }
-    std::cout << "NO\n";
+    cout << "NO\n";
 
     return 0;
 }
