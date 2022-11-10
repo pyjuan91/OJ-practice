2406486734ad712f0502cbb7452588fc8ec23878
poyu <yoyohihirobert@gmail.com>
2022-11-10 16:46:07 +0800

    mid h tmp

---
+++ meow/midterm-H.cc
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    while (cin >> n && n) {
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        
+    }
+    return 0;
+}
