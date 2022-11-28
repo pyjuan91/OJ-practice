9295787846377c571425a8db8f6249e40ed98108
Poyu Juan <yoyohihirobert@gmail.com>
2022-11-28 14:44:33 +0800

    meow midterm H accept

--- meow/midterm-H.cc
+++ meow/midterm-H.cc
@@ -11,22 +11,21 @@ vector<int> a(70);
 int n;
 int target;
 int len;
-bool solve(int done, int now, int last)
+bool solve(int finish_sticks, int now_len, int last)
 {
-    if (done == target)
+    if (finish_sticks >= target)
         return true;
-    if (now == len)
-        return solve(done + 1, 0, 1);
-    int fail = -1;
+    if (now_len == len)
+        return solve(finish_sticks + 1, 0, 1);
     for (int i = last; i <= n; i++) {
-        if (used[i] || now + a[i] > len || a[i] == fail)
+        if (used[i] || now_len + a[i] > len)
             continue;
         used[i] = true;
-        if (solve(done, now + a[i], i + 1))
+        if (solve(finish_sticks, now_len + a[i], i + 1))
             return true;
         used[i] = 0;
-        a[i] = fail;
-        if(now == 0 || now + a[i] == len)return false;
+        if (now_len == 0|| now_len + a[i] == len)
+            return false;
     }
     return false;
 }
@@ -35,13 +34,13 @@ int main(void)
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     while (cin >> n && n) {
-        int res = 0, ma = INT_MIN, sum = 0;
+        int ma = INT_MIN, sum = 0;
         for (int i = 1; i <= n; i++) {
             cin >> a[i];
             sum += a[i];
         }
-        sort(a.begin(), a.end(), greater<int>());
-        ma = a[0];
+        sort(a.begin() + 1, a.begin() + 1 + n, greater<int>());
+        ma = a[1];
         for (int i = ma; i <= sum; i++) {
             if (sum % i == 0) {
                 target = sum / i;
--- test.cpp
+++ test.cpp
@@ -1,107 +1,74 @@
 #include <algorithm>
-#include <climits>
+#include <cstdio>
 #include <cstring>
-#include <fstream>
+#include <functional>
 #include <iostream>
-#include <queue>
 #include <vector>
-#define pii pair<int, int>
-#define mn 1005
+
 using namespace std;
+const int maxn = 100;
 
-int N, M;
-queue<pii> q;
-int paths[mn][mn];
-pii from[mn][mn];
-int oo = INT_MAX;
-pii A;
-string ans;
-bool possible = false;
+vector<int> s(maxn);
+vector<int> vis(maxn);
 
-void retrace(pii node)
-{ // retrace from final node, adding direction from previous node to a string. This string will be backwards but will be reversed before output.
-    pii origin = from[node.first][node.second];
-    if (origin == pii(0, 0))
-        return;
-    if (origin.first == node.first + 1)
-        ans.push_back('U');
-    if (origin.first == node.first - 1)
-        ans.push_back('D');
-    if (origin.second == node.second + 1)
-        ans.push_back('L');
-    if (origin.second == node.second - 1)
-        ans.push_back('R');
-    retrace(origin);
-}
-void check(pii origin, pii dest)
-{ // check if the considered destination may be traveled to
-    int pl = paths[origin.first][origin.second];
-    if (pl + 1 < paths[dest.first][dest.second]) {
-        paths[dest.first][dest.second] = pl + 1;
-        q.push(dest);
-        from[dest.first][dest.second] = origin;
-    }
-}
-bool mora = false; // false if bfs for monsters, true if bfs for A
-void bfs()
+bool cmp(int a, int b)
 {
-    while (!q.empty()) {
-        pii loc = q.front(), next;
-        q.pop();
-        next = loc;
-        next.first++;
-        check(loc, next); // go through adjacent locations
-        next = loc;
-        next.first--;
-        check(loc, next);
-        next = loc;
-        next.second++;
-        check(loc, next);
-        next = loc;
-        next.second--;
-        check(loc, next);
-        if (mora && (loc.first == 1 || loc.second == 1 || loc.first == N || loc.second == M)) {
-            cout << "YES" << endl;
-            cout << paths[loc.first][loc.second] << endl;
-            retrace(loc);
-            possible = true;
-            return;
+    return a > b;
+}
+
+int num, len;
+int n;
+
+bool dfs(int done, int now, int last)
+{
+    if (done >= num)
+        return true;
+    if (now == len)
+        return dfs(done + 1, 0, 1);
+    int fail = 0; //剪枝1：不重复插入上一个失败的木棍长度
+    for (int i = last; i <= n; i++) {
+        //剪枝2：构造一根原始木棍时按递减插入，
+        // 因为先插入x后插入y和先插入y后插入x是一样的
+        if (!vis[i] && now + s[i] <= len /*&& s[i] != fail*/) {
+            vis[i] = 1;
+            if (dfs(done, now + s[i], i + 1))
+                return true;
+            vis[i] = 0;
+            /*fail = s[i];*/
+            if (now == 0 || now + s[i] == len)
+                return false;
+            //剪枝3，4.如果当前木棍是0或当前木棍以拼成功，却失败了，则说明无解
         }
     }
+    return false;
 }
+
 int main()
 {
-//     fstream fin;
-//     fin.open("input.txt", ios::in);
-// #define cin fin
-
-    cin >> N >> M;
-    for (int i = 1; i <= N; i++) {
-        string s;
-        cin >> s;
-        for (int j = 1; j <= M; j++) {
-            paths[i][j] = oo;
-            if (s[j - 1] == '#')
-                paths[i][j] = 0;
-            if (s[j - 1] == 'M') {
-                q.push(pii(i, j));
-                paths[i][j] = 0;
-            }
-            if (s[j - 1] == 'A') {
-                A.first = i;
-                A.second = j;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    while (cin >> n && n) {
+        int to = 0;
+        int maxx = 0;
+        for (int i = 1; i <= n; i++) {
+            cin >> s[i];
+            to += s[i];
+            // maxx = max(maxx, s[i]);
+        }
+        sort(s.begin() + 1, s.begin() + n + 1, greater<int>());
+        maxx = s[1];
+        for (int i = maxx; i <= to; i++) {
+            if (to % i == 0) {
+                len = i;
+                num = to / i;
+                // memset(vis, 0, sizeof(vis));
+                fill(vis.begin(), vis.end(), false);
+                if (dfs(0, 0, 1)) {
+                    cout << i << "\n";
+                    break;
+                }
             }
         }
     }
-    bfs(); // monster bfs
-    mora = true; // change next bfs to A bfs
-    from[A.first][A.second] = pii(0, 0); // give the retrace a terminating location
-    paths[A.first][A.second] = 0;
-    q.push(A); // get ready for next bfs
-    bfs(); // bfs with A
-    if (possible) {
-        reverse(ans.begin(), ans.end());
-        cout << ans << endl;
-    } else
-        cout << "NO" << endl;
+    return 0;
 }
