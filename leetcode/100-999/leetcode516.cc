9d98f8ae337781816b3eba3fecaab03864fbace1
Poyu Juan <yoyohihirobert@gmail.com>
2022-12-05 20:18:12 +0800

    .

--- leetcode/100-999/leetcode516.cc
+++ leetcode/100-999/leetcode516.cc
@@ -11,19 +11,15 @@ public:
             return dp[left][right];
         if (left == right)
             return dp[left][right] = 1;
-        else if (s[left] == s[right]) {
+        if (s[left] == s[right])
             return dp[left][right] = 2 + solve(s, left + 1, right - 1);
-        } else {
-            return dp[left][right]
-                = max(solve(s, left + 1, right), solve(s, left, right - 1));
-        }
+        // s[left] != s[right]
+        return dp[left][right]
+            = max(solve(s, left + 1, right), solve(s, left, right - 1));
     }
     int longestPalindromeSubseq(string s)
     {
-        int len = s.length();
-        for (int i = 1; i <= len; i++)
-            dp[i][i] = 1;
-        return solve(s, 0, len - 1);
+        return solve(s, 0, s.length() - 1);
     }
 };
 int main(void) { return 0; }
