48f1f2616e0ce3e79cfb7f46791aaf3b57ef6500
Po-Yu Juan <yoyohihirobert@gmail.com>
2023-02-04 22:02:35 +0800

    atcoder abc288a

---
+++ atcoder/abc288a.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie();
+	int n, a, b;
+	cin >> n;
+	while (n--) {
+		cin >> a >> b;
+		cout << a + b << "\n";
+	}
+	return 0;
+}
