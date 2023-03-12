9d0bbbd44766c8f83dea43e3f72028c87350b542
Po-Yu Juan <yoyohihirobert@gmail.com>
2023-03-12 16:12:06 +0800

    figuring

--- leetcode/10-99/leetcode32.cc
+++ leetcode/10-99/leetcode32.cc
@@ -1,22 +1,24 @@
 #include <iostream>
 using namespace std;
 class Solution {
-public:
-    int dp[30005] = {};
-    int longestValidParentheses(string s)
-    {
-        int len = s.length(), res = 0;
-        // dp: end at i's longest valid parentheses
-        for (int i = 1; i < len; i++) {
-            if (s[i] == ')') {
-                int last_start = i - dp[i - 1];
-                if (last_start && s[last_start - 1] == '(') {
-                    dp[i] = dp[i - 1] + 2 + (last_start - 1 ? dp[last_start - 2] : 0);
-                    res = max(res, dp[i]);
-                }
-            }
-        }
-        return res;
-    }
+	public:
+		int dp[30005] = {};
+		int longestValidParentheses(string s) {
+			int len = s.length(), res = 0;
+			// dp: end at i's longest valid parentheses
+			for (int i = 1; i < len; i++) {
+				if (s[i] == ')') {
+					int last_start = i - dp[i - 1];
+					if (last_start && s[last_start - 1] == '(') {
+						dp[i] = dp[i - 1] + 2
+							  + (last_start - 1
+								   ? dp[last_start - 2]
+								   : 0);
+						res = max(res, dp[i]);
+					}
+				}
+			}
+			return res;
+		}
 };
 int main(void) { return 0; }
--- test.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(void) {
-    int c;
-    // cin >> c;
-    cout << "Hi VScode" << c << endl;
-    return 0;
-}
