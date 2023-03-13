b450125fc588f2e104163e74a4a58e273f57394e
Po-Yu Juan <yoyohihirobert@gmail.com>
2023-03-14 02:53:49 +0800

    atcoder dpi

--- atcoder/dpi.cc
+++ atcoder/dpi.cc
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-double dp[3000][3000] = {0.0}, p;
+double dp[3000] = {0.0}, p;
 int N;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie();
 	cin >> N;
-	dp[0][0] = 1;
+	dp[0] = 1;
 	for (int i = 1; i <= N; i++) {
 		cin >> p;
-		dp[i][0] = (1 - p) * dp[i - 1][0];
-		for (int j = 1; j <= i; j++)
-			dp[i][j] += dp[i - 1][j] * (1 - p) + dp[i - 1][j - 1] * p;
+		for (int j = i; j >= 1; j--)
+			dp[j] = dp[j] * (1 - p) + dp[j - 1] * p;
+		dp[0] = (1 - p) * dp[0];
 	}
 	p = 0;
-	for (int i = N / 2 + 1; i <= N; i++) p += dp[N][i];
+	for (int i = N / 2 + 1; i <= N; i++) p += dp[i];
 	cout << setprecision(20) << p;
 	return 0;
 }
