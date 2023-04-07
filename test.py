e13acf3cec89ddeed03b7ddb84d06d51ad406a9e
Po-Yu Juan <yoyohihirobert@gmail.com>
2023-04-07 20:11:48 +0800

    atcoder abc272d

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-inputList = []
-while True:
-    try:
-        inputList.append(input())
-    except EOFError as e:
-        break
-
-inputList.append('Sky')
-inputList.append('Limit')
-inputList.append('Fly')
-
-print(f'{inputList[0]}{inputList[1]}{inputList[2]}{inputList[-3]}{inputList[-2]}{inputList[-1]}')
-
+print("Hello, world")
