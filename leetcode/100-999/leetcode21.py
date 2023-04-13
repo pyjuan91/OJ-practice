5630d9a782e667fb284f5191b1486589d1ce8a67
Po-Yu Juan <yoyohihirobert@gmail.com>
2023-04-13 21:39:39 +0800

    leetcode 21

---
+++ leetcode/100-999/leetcode21.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+from typing import Optional
+
+
+class Solution:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        head = res = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val:
+                res.next = list1
+                list1 = list1.next
+            else:
+                res.next = list2
+                list2 = list2.next
+            res = res.next
+        if list1:
+            res.next = list1
+        if list2:
+            res.next = list2
+        return head.next
