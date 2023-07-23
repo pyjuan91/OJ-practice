6c15f7749057b690c9adccbfe18af2e3c892f246
Po-Yu Juan <pyjuan91@gmail.com>
2023-07-24 00:17:27 +0800

    atcoder abc045a

---
+++ atcoder/abc045_a.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+int32_t main() {
+	cin.tie(nullptr)->sync_with_stdio(false);
+	int a, b, h;
+	cin >> a >> b >> h;
+	cout << (a + b) * h / 2;
+	return 0;
+}
