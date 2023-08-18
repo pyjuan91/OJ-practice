e64a83aa021cb4cd7b0bef516b93949364d80a65
Po-Yu Juan <pyjuan91@gmail.com>
2023-08-19 03:09:02 +0800

    cf 1860 d

--- codeforces/1860d.cc
+++ codeforces/1860d.cc
@@ -1,42 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+int dp[101][101][5001];
+string s;
+int cnt_0, cnt_1;
+int chaewon(int pre_0, int pre_1, int balance) {
+	if (pre_0 > cnt_0 || pre_1 > cnt_1) return INT32_MAX - 1;
+	if (pre_0 == cnt_0 && pre_1 == cnt_1 && balance == 2500)
+		return 0;
+	if (dp[pre_0][pre_1][balance] != -1)
+		return dp[pre_0][pre_1][balance];
+	if (s[pre_0 + pre_1] == '0')
+		return dp[pre_0][pre_1][balance] = min(
+				 chaewon(pre_0 + 1, pre_1, balance - pre_1),
+				 chaewon(pre_0, pre_1 + 1, balance + pre_0) + 1);
+	return dp[pre_0][pre_1][balance] = min(
+			 chaewon(pre_0, pre_1 + 1, balance + pre_0),
+			 chaewon(pre_0 + 1, pre_1, balance - pre_1) + 1);
+}
 int32_t main() {
 	cin.tie(nullptr)->sync_with_stdio(false);
-	string s;
+	memset(dp, -1, sizeof(dp));
 	cin >> s;
-	int len = s.length();
-	auto value = [&](string& s) {
-		int left_zero[len] = {}, right_zero[len] = {};
-		for (int i = 0; i < len; i++) {
-			if (i) left_zero[i] = left_zero[i - 1];
-			if (s[i] == '0') left_zero[i]++;
-		}
-		for (int i = len - 1; i >= 0; i--) {
-			if (i != len - 1) right_zero[i] = right_zero[i + 1];
-			if (s[i] == '0') right_zero[i]++;
-		}
-		int oI = 0, Io = 0;
-		for (int i = 0; i < len; i++) {
-			if (s[i] == '1') {
-				oI += left_zero[i];
-				Io += right_zero[i];
-			}
-		}
-		return abs(oI - Io);
-	};
-	int res = 0;
-	while (value(s) != 0) {
-		string nice = s;
-		for (int i = 0; i < len; i++) {
-			for (int j = i + 1; j < len; j++) {
-				swap(s[i], s[j]);
-				if (value(s) < value(nice)) nice = s;
-				swap(s[i], s[j]);
-			}
-		}
-		s = nice;
-		res++;
+	cnt_0 = cnt_1 = 0;
+	for (auto x : s) {
+		if (x == '0') cnt_0++;
+		else cnt_1++;
 	}
-    cout << res << '\n';
+	cout << chaewon(0, 0, 2500) / 2 << '\n';
 	return 0;
 }
