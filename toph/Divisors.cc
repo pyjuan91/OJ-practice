4816f40a953f887383aaa64437fca07fe878c459
Po-Yu Juan <pyjuan91@gmail.com>
2023-09-03 16:01:13 +0800

    toph divisors

---
+++ toph/Divisors.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int32_t main() {
+	cin.tie(nullptr)->sync_with_stdio(false);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		if (n % i == 0) cout << i << '\n';
+	}
+	return 0;
+}
