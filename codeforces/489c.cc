1eb10c0443eaa9712f855b71ca70d5642b910549
Po-Yu Juan <pyjuan91@gmail.com>
2023-09-25 22:04:32 +0800

    cf 489c

--- codeforces/489c.cc
+++ codeforces/489c.cc
@@ -1,33 +1,47 @@
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
 int32_t main() {
 	cin.tie(nullptr)->sync_with_stdio(false);
-	int d, s;
-	cin >> d >> s;
-	vector<int> smallest(d, 0), greatest(d, 9);
-	smallest[0] = 1;
-	int smallestSum = 1, greatestSum = 9 * d;
-	if (smallestSum > s || greatestSum < s) {
+	int m, s;
+	cin >> m >> s;
+	if (m == 1) {
+		if (s >= 0 && s <= 9) cout << s << " " << s << "\n";
+		else cout << "-1 -1\n";
+		return 0;
+	}
+	string mi = "", ma = "";
+	int mis = 1, mas = 9 * m;
+	for (int i = 0; i < m; i++)
+		mi.push_back('0'), ma.push_back('9');
+	mi[0] = '1';
+	// modify mi
+	if (mis > s || mas < s) {
 		cout << "-1 -1\n";
 		return 0;
 	}
-	for (int i = d - 1; i >= 0; i--) {
-		if (smallestSum != s) {
-			int diff = s - smallestSum;
-			if (diff >= 9) smallest[i] = 9, smallestSum += 9;
-			else smallest[i] += diff, smallestSum += diff;
+	for (int i = m - 1; i >= 0; i--) {
+		int diff = s - mis;
+		if (diff >= 9) {
+			mi[i] = '9';
+			mis += 9;
+		}
+		else {
+			mi[i] += diff;
+			mis += diff;
 		}
 	}
-	for (int i = d - 1; i >= 0; i--) {
-		if (greatestSum != s) {
-			int diff = greatestSum - s;
-			if (diff >= 9) greatest[i] = 0, greatestSum -= 9;
-			else greatest[i] -= diff, greatestSum -= diff;
+	// modify ma
+	for (int i = m - 1; i >= 0; i--) {
+		int diff = mas - s;
+		if (diff >= 9) {
+			ma[i] = '0';
+			mas -= 9;
+		}
+		else {
+			ma[i] -= diff;
+			mas -= diff;
 		}
 	}
-	for (int i = 0; i < d; i++) cout << smallest[i];
-	cout << " ";
-	for (int i = 0; i < d; i++) cout << greatest[i];
+	cout << mi << " " << ma << "\n";
 	return 0;
 }
