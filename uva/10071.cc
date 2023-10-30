b9c62d76c47a3e99206c44d19fa7479f0f27fc98
Po-Yu Juan <pyjuan91@gmail.com>
2023-10-30 17:24:13 +0800

    uva 10071

---
+++ uva/10071.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+int32_t main() {
+	cin.tie(nullptr)->sync_with_stdio(false);
+	int a, b;
+	while (cin >> a >> b) cout << a * b * 2 << '\n';
+	return 0;
+}
