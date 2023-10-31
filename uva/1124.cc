e068ec47be7410503f16f59477660361bfc1c26a
Po-Yu Juan <pyjuan91@gmail.com>
2023-10-31 13:04:50 +0800

    uva 1124

---
+++ uva/1124.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+int32_t main() {
+	cin.tie(nullptr)->sync_with_stdio(false);
+	string s;
+	while (getline(cin, s)) cout << s << '\n';
+	return 0;
+}
