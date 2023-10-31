434e57e1e2fbfbafff3bc57142171f43e5ea77fc
Po-Yu Juan <pyjuan91@gmail.com>
2023-10-31 12:53:51 +0800

    uva 13025

---
+++ uva/13025.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+int32_t main() {
+	cin.tie(nullptr)->sync_with_stdio(false);
+	// 10/30 -> Wendsday
+    // 10/29 -> Tuesday
+    // 10/28 -> Monday
+    // ...
+    // 10/1 -> Tuesday
+    // 9/1 -> Sunday
+    // 8/1 -> Friday
+    // 7/1 -> Tuesday
+    // 6/1 -> Sunday
+    // 5/31 -> Friday
+    // 5/30 -> Thursday
+    // 5/29 -> Wednesday
+	std::cout << "May 29, 2013 Wednesday\n";
+	return 0;
+}
