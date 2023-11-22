03c10183e463bdbec49d3bf9b27c048f4d4ac84d
Po-Yu Juan <pyjuan91@gmail.com>
2023-11-22 16:59:29 +0800

    vj

---
+++ vj/the_boat_filled_with_love.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+int32_t main() {
+	cin.tie(nullptr)->sync_with_stdio(false);
+	string s;
+	cin >> s;
+	cout << "We Love You. We " << s << " Are Here For You.\n";
+	return 0;
+}
