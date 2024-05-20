0f56064bb246efdcd25c0c2c81795c6276a75d5f
Po-Yu Juan <pyjuan91@gmail.com>
2024-05-20 16:32:08 +0800

    template update

--- template.cc
+++ template.cc
@@ -1,4 +1,10 @@
 #include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#include "./debug.cc"
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
 #define int long long
 using namespace std;
 int32_t main() {
