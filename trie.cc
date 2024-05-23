80661bb6ad91107f4e258d78764777214a6babf8
Po-Yu Juan <pyjuan91@gmail.com>
2024-05-23 14:30:47 +0800

    template udpate

--- trie.cc
+++ trie.cc
@@ -1,46 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Trie {
+// Trie node class
+class TrieNode {
  public:
-  static const int kMax = 5e5 + 1;
-  int** nex;
-  int* exist;
-  int cnt;
-
-  Trie() {
-    nex = new int*[kMax];
-    for (int i = 0; i < kMax; i++) {
-      nex[i] = new int[26];
-      memset(nex[i], 0, sizeof(int) * 26);
+  // A map to store children nodes
+  std::unordered_map<char, std::unique_ptr<TrieNode>> children;
+  // Flag to mark the end of a word
+  bool isEndOfWord = false;
+  // Method to get the next TrieNode given a character
+  TrieNode* getNextNode(char c) {
+    if (children.count(c)) {
+      return children[c].get();
     }
-    exist = new int[kMax];
-    memset(exist, 0, sizeof(int) * kMax);
-    cnt = 0;
+    return nullptr;
+  }
+};
+
+// Trie class
+class Trie {
+ private:
+  std::unique_ptr<TrieNode> root;
+
+ public:
+  // Constructor
+  Trie() : root(std::make_unique<TrieNode>()) {}
+
+  TrieNode* getRoot() {
+    return root.get();
+  }
+  // Function to insert a word into the Trie
+  void insert(const std::string& word) {
+    TrieNode* node = root.get();
+    for (char ch : word) {
+      if (!node->children.count(ch)) {
+        node->children[ch] = std::make_unique<TrieNode>();
+      }
+      node = node->children[ch].get();
+    }
+    node->isEndOfWord = true;
   }
 
-  void insert(const string& s) {
-    int p = 0;
-    for (int i = 0; i < s.size(); i++) {
-      int c = s[i] - 'a';
-      if (!nex[p][c]) nex[p][c] = ++cnt;
-      p = nex[p][c];
+  // Function to search for a word in the Trie
+  bool search(const std::string& word) const {
+    TrieNode* node = root.get();
+    for (char ch : word) {
+      if (!node->children.count(ch)) {
+        return false;
+      }
+      node = node->children[ch].get();
     }
-    exist[p] = 1;
+    return node->isEndOfWord;
   }
 
-  int find(const string& s) {
-    int p = 0;
-    for (int i = 0; i < s.size(); i++) {
-      int c = s[i] - 'a';
-      if (!nex[p][c]) return 0;
-      p = nex[p][c];
+  // Function to check if there is any word in the Trie that starts with the
+  // given prefix
+  bool startsWith(const std::string& prefix) const {
+    TrieNode* node = root.get();
+    for (char ch : prefix) {
+      if (!node->children.count(ch)) {
+        return false;
+      }
+      node = node->children[ch].get();
     }
-    if (!exist[p]) return 0;
-    if (exist[p] == 1) {
-      exist[p] = 2;
-      return 1;
-    }
-    return -1;
+    return true;
   }
 };
