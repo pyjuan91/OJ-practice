e7a7bc187ca4d2301a2e5d2a115aa50b671421af
Po-Yu Juan <pyjuan91@gmail.com>
2025-06-11 09:00:28 +0800

    cf 2104e

--- codeforces/2104e.cc
+++ codeforces/2104e.cc
@@ -9,61 +9,35 @@
 using namespace std;
 int32_t main() {
   cin.tie(nullptr)->sync_with_stdio(false);
-  int n, k;
-  string s;
-  cin >> n >> k >> s;
+  int t, n;
+  cin >> t;
 
-  vector<vector<int>> pos(k);
-  for (int i = 0; i < n; i++) {
-    pos[s[i] - 'a'].push_back(i + 1);
-  }
+  auto ask = [&](string query, int x) -> int {
+    if (query == "digit")
+      cout << query << endl;
+    else
+      cout << query << " " << x << endl;
+    cout.flush();
+    int res;
+    cin >> res;
+    return res;
+  };
 
-  vector<int> dp(n + 5), next_pos(k);
+  auto answer = [&]() -> int {
+    cout << "!" << endl;
+    cout.flush();
+    int x;
+    cin >> x;
+    return x;
+  };
 
-
-  for (int i = n; i >= 1; i--) {
-    int c = s[i - 1] - 'a';
-    next_pos[c] = i;
-
-    bool all_appear = true;
-    for (int c = 0; c < k; c++) {
-      if (next_pos[c] == 0) {
-        all_appear = false;
-        break;
-      }
-    }
-    if (!all_appear) {
-      dp[i] = 0;
-      continue;
-    }
-    else {
-      int mi = INT64_MAX;
-      for (int c = 0; c < k; c++) {
-        int p = next_pos[c] + 1;
-        mi = min(mi, dp[p]);
-      }
-      dp[i] = mi + 1;
-    }
-  }
-
-  int q;
-  cin >> q;
-  string t;
-  while (q--) {
-    cin >> t;
-    int current_pos = 1;
-    bool is_sub = true;
-    for (auto ch : t) {
-      const auto& v = pos[ch - 'a'];
-      auto it = lower_bound(v.begin(), v.end(), current_pos);
-      if (it == v.end()) {
-        is_sub = false;
-        break;
-      }
-      current_pos = *it + 1;
-    }
-
-    cout << (not is_sub ? 0 : dp[current_pos] + 1) << "\n";
+  while (t--) {
+    cin >> n;
+    ask("mul", 9);
+    ask("digit", 0);
+    ask("digit", 0);
+    ask("add", n - 9);
+    answer();
   }
   return 0;
 }
