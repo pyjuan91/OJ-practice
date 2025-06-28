7d6181bad46ef47b3cc1796ed1b29fbd94ae2e57
Po-Yu Juan <pyjuan91@gmail.com>
2025-06-28 22:55:39 +0800

    lc 1049

---
+++ 0x3F/1049.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+  public:
+    int lastStoneWeightII(vector<int> &stones)
+    {
+        // [a, b, c, d] -> [a, b, (d-c)] -> [(b-d+c), a] -> [(b-d+c-a)];
+        int sum = accumulate(stones.begin(), stones.end(), 0);
+        vector<bool> dp(sum + 1);
+        dp[0] = true;
+        for (auto stone : stones)
+        {
+            for (int i = sum; i >= stone; i--)
+                dp[i] = dp[i] || dp[i - stone];
+        }
+        for (int plus = sum / 2 + sum % 2; plus <= sum; plus++)
+        {
+            if (dp[plus])
+                return 2 * plus - sum;
+        }
+        return 0;
+    }
+};
