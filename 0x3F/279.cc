f73cf1cf68c164cdfee2b5157a71c44de9732db6
Po-Yu Juan <pyjuan91@gmail.com>
2025-08-24 01:34:11 -0400

    lc 279

---
+++ 0x3F/279.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int numSquares(int n)
+    {
+        vector<int> dp(n + 1, INT32_MAX >> 1);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= static_cast<int>(sqrt(i)); j++)
+            {
+                dp[i] = min(dp[i], 1 + dp[i - j * j]);
+            }
+        }
+        return dp[n];
+    }
+};
+
+int main() { return 0; }
