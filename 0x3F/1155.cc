b65d3beeb9a2286003aefa83c22c2955a5db7098
Po-Yu Juan <pyjuan91@gmail.com>
2025-08-31 00:55:20 -0400

    lc 1155

--- 0x3F/1155.cc
+++ 0x3F/1155.cc
@@ -6,24 +6,28 @@ class Solution
 public:
     int numRollsToTarget(int n, int k, int target)
     {
+        if (target < n or target > n * k)
+        {
+            return 0;
+        }
         const int kMod = 1e9 + 7;
-        vector<int> dp(target + 1, 0);
+        vector<int> dp(target - n + 1, 0);
         dp[0] = 1;
 
         for (int dice_idx = 1; dice_idx <= n; dice_idx++)
         {
-            vector<int> dp_next(target + 1, 0);
-            for (int face_number = 1; face_number <= k; face_number++)
+            int max_j = min(target - n, dice_idx * (k - 1));
+            for (int j = 1; j <= max_j; j++)
             {
-                for (int sum = target; sum >= face_number; sum--)
-                {
-                    dp_next[sum] = (dp_next[sum] + dp[sum - face_number]) % kMod;
-                }
+                dp[j] = (dp[j] + dp[j - 1]) % kMod;
             }
-            dp.swap(dp_next);
+            for (int j = max_j; j >= k; j--)
+            {
+                dp[j] = (dp[j] - dp[j - k] + kMod) % kMod;
+            }
         }
 
-        return dp[target];
+        return dp[target - n];
     }
 };
 
