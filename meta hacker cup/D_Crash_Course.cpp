66f688496790134466eef6e3f16630df522d980f
Po-Yu Juan <pyjuan91@gmail.com>
2025-10-29 12:24:38 -0400

    meta hacker cup crash course

--- meta hacker cup/D_Crash_Course.cpp
+++ meta hacker cup/D_Crash_Course.cpp
@@ -1,43 +1,28 @@
 #include <bits/stdc++.h>
 #ifdef LOCAL
-#include "./debug.cc"
+#	include "./debug.cc"
 #else
-#define debug(...)
-#define debugArr(...)
+#	define debug(...)
+#	define debugArr(...)
 #endif
 #define int long long
 using namespace std;
-
-string patty(int n, string& s)
-{
-    if (s.back() == 'A')
-    {
-        return "Alice";
-    }
-    deque<char> dq(s.begin(), s.end());
-    while (!dq.empty() && dq.front() == 'B')
-    {
-        dq.pop_front();
-    }
-    int count_A = count(dq.begin(), dq.end(), 'A');
-    int count_B = dq.size() - count_A;
-    if (count_A > count_B)
-    {
-        return "Alice";
-    }
-    return "Bob";
-}
-
-int32_t main()
-{
-    cin.tie(nullptr)->sync_with_stdio(false);
-    int t, n;
-    string s;
-    cin >> t;
-    for (int kase = 1; kase <= t; kase++)
-    {
-        cin >> n >> s;
-        cout << "Case #" << kase << ": " << patty(n, s) << "\n";
-    }
-    return 0;
+int32_t main() {
+	cin.tie(nullptr)->sync_with_stdio(false);
+	int t, n;
+	string s;
+	cin >> t;
+	for (int kase = 1; kase <= t; kase++) {
+		cin >> n >> s;
+		bool alice_win = false;
+		int cnt[2] = {};
+		for (int i = n - 1; i >= 0; i--) {
+			cnt[s[i] - 'A']++;
+			if (cnt[0] > cnt[1]) {
+				alice_win = true;
+			}
+		}
+		cout << "Case #" << kase << ": " << (alice_win ? "Alice" : "Bob") << "\n";
+	}
+	return 0;
 }
