83787a53f683a1366f44da8ded2971c2aecab355
Po-Yu Juan <pyjuan91@gmail.com>
2025-12-30 17:40:53 +0800

    lc 437

--- 0x3F/437.cc
+++ 0x3F/437.cc
@@ -15,24 +15,21 @@ class Solution {
  public:
   int pathSum(TreeNode* root, int targetSum) {
     if (!root) return 0;
-    int res_path_sum = 0;
-    auto dfs = [&](this const auto& self,
-                   TreeNode* root) -> unordered_map<int64_t, int64_t> {
-      unordered_map<int64_t, int64_t> res, lc, rc;
-      if (root->left) lc = self(root->left);
-      if (root->right) rc = self(root->right);
-      for (const auto& [k, v] : lc) {
-        res[k + root->val] += v;
-      }
-      for (const auto& [k, v] : rc) {
-        res[k + root->val] += v;
-      }
-      res[root->val] += 1;
-      if (res.count(targetSum)) res_path_sum += res[targetSum];
-      return res;
+    int64_t res_path_sum = 0, cur = 0;
+    unordered_map<int64_t, int64_t> cnt;
+    auto dfs = [&](this const auto& self, TreeNode* root) -> void {
+      if (root == nullptr) return;
+      cur += root->val;
+      if (cur == targetSum) res_path_sum++;
+      if (cnt.count(cur - targetSum)) res_path_sum += cnt[cur - targetSum];
+      cnt[cur]++;
+      if (root->left) self(root->left);
+      if (root->right) self(root->right);
+      cnt[cur]--;
+      cur -= root->val;
     };
     dfs(root);
-    return res_path_sum;
+    return static_cast<int32_t>(res_path_sum);
   }
 };
 
