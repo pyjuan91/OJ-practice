5dc70dceb6eca4e955c908beeeabd0facaa4fc7b
Po-Yu Juan <pyjuan91@gmail.com>
2026-02-09 00:36:23 -0500

    lc 1534

---
+++ 0x3F/1534.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+    const int n = arr.size();
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (abs(arr[i] - arr[j]) <= a) {
+          for (int k = j + 1; k < n; k++) {
+            if (abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c) res++;
+          }
+        }
+      }
+    }
+    return res;
+  }
+};
+
+int main() { return 0; }
