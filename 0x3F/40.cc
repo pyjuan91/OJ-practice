0a02e560d6e56c1a20e7b6152a6ee410a3832f97
Poe Juan <pyjuan91@gmail.com>
2026-08-03 19:02:35 +0800

    lc 40

--- 0x3F/40.cc
+++ 0x3F/40.cc
@@ -6,29 +6,35 @@ class Solution {
   vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
     vector<vector<int>> res;
     vector<int> cur_candidates;
-    map<int, int> cnt;
-    for (const auto& candidate : candidates) cnt[candidate]++;
+    vector<pair<int, int>> groups;
+    {
+      map<int, int> cnt_;
+      for (const auto& x : candidates) cnt_[x]++;
+      groups.assign(cnt_.begin(), cnt_.end());
+    }
     int sum = 0;
 
-    auto dfs = [&](this auto&& self, map<int, int>::iterator it) -> void {
+    auto dfs = [&](this auto&& self, size_t idx) -> void {
       if (sum == target) {
         res.push_back(cur_candidates);
         return;
       }
-      if (it == cnt.end()) return;
-      for (int i = 1; i <= it->second; i++) {
-        if (sum + it->first * i > target) break;
-        for (int _ = 0; _ < i; _++) cur_candidates.push_back(it->first);
-        sum += it->first * i;
-        self(next(it));
-        for (int _ = 0; _ < i; _++) cur_candidates.pop_back();
-        sum -= it->first * i;
+      if (idx == groups.size()) return;
+      int num = groups[idx].first, cnt = groups[idx].second;
+      int i = 1;
+      for (; i <= cnt and sum + num <= target; i++) {
+        cur_candidates.push_back(num);
+        sum += num;
+        self(idx + 1);
       }
-      self(next(it));
+      i--;
+      cur_candidates.resize(cur_candidates.size() - i);
+      sum -= num * i;
+      self(idx + 1);
     };
 
-    dfs(cnt.begin());
-    return vector<vector<int>>(res.begin(), res.end());
+    dfs(0);
+    return res;
   }
 };
 
