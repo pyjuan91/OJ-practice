0a61d2520ff9b4671732a9b00ca0dc4af211bca3
Poe Juan <pyjuan91@gmail.com>
2026-09-20 13:14:32 +0800

    lc 3498

---
+++ 0x3F/3498.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  int reverseDegree(string s) {
+    int res = 0;
+    for (int i = 0; i < s.size(); i++) {
+        res += (i + 1) * (26 - s[i] + 'a');
+    }
+    return res;
+  }
+};
+
+int main() { return 0; }
